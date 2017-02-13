9e3df43e56d9bc76bb2e7f0a31c7b84ed7754846
Ankit Asthana <aasthan@microsoft.com>
2017-02-12 23:11:58 -0800

    .

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
- 
 
+g++ -std=c++11 -I/imagecppservice/azure-storage-cpp/Microsoft.WindowsAzure.Storage/includes -I/imagecppservice/casablanca/Release/include -L/imagecppservice/azure-storage-cpp/Microsoft.WindowsAzure.Storage/build.release/Binaries/  -lazurestorage -L/imagecppservice/casablanca/Release/build.release/Binaries -lcpprest -lboost_system -lcrypto -lssl -lz -L/imagecppservice/opencv-3.1.0/build/lib -lopencv_core -lopencv_imgproc -lopencv_imgcodecs imagefilters.cpp 
 
-
-export LD_LIBRARY_PATH=
-
-g++ -std=c++11 -I/crimecop/azure-storage-cpp/Microsoft.WindowsAzure.Storage/includes -I/crimecop/azure-storage-cpp/Microsoft.WindowsAzure.Storage/casablanca/Release/include -L/crimecop/azure-storage-cpp/Microsoft.WindowsAzure.Storage/build.release/Binaries/ `pkg-config opencv --cflags --libs` -lazurestorage -L/crimecop/azure-storage-cpp/Microsoft.WindowsAzure.Storage/casablanca/Release/build.release/Binaries -lcpprest -lboost_system -lcrypto -lssl -lz imagefilters.cpp
-
-
+LD_LIBRARY_PATH=//imagecppservice/azure-storage-cpp/Microsoft.WindowsAzure.Storage/build.release/Binaries:/imagecppservice/casablanca/Release/build.release/Binaries:/imagecppservice/opencv-3.1.0/build/lib
