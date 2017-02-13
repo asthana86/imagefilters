931b3c77a9d208f5c8e9d49240201ff839d8518e
Ankit Asthana <aasthan@microsoft.com>
2017-02-13 09:08:06 -0800

    adding export to LD_LIBRARY_PATH

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
 g++ -std=c++11 -I/imagecppservice/azure-storage-cpp/Microsoft.WindowsAzure.Storage/includes -I/imagecppservice/casablanca/Release/include -L/imagecppservice/azure-storage-cpp/Microsoft.WindowsAzure.Storage/build.release/Binaries/  -lazurestorage -L/imagecppservice/casablanca/Release/build.release/Binaries -lcpprest -lboost_system -lcrypto -lssl -lz -L/imagecppservice/opencv-3.1.0/build/lib -lopencv_core -lopencv_imgproc -lopencv_imgcodecs imagefilters.cpp 
 
-LD_LIBRARY_PATH=//imagecppservice/azure-storage-cpp/Microsoft.WindowsAzure.Storage/build.release/Binaries:/imagecppservice/casablanca/Release/build.release/Binaries:/imagecppservice/opencv-3.1.0/build/lib
+export LD_LIBRARY_PATH=/imagecppservice/azure-storage-cpp/Microsoft.WindowsAzure.Storage/build.release/Binaries:/imagecppservice/casablanca/Release/build.release/Binaries:/imagecppservice/opencv-3.1.0/build/lib
